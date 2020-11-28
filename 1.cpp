99c63a56db775a8eec864acd331276f7217bd456
Dexy Arya <38392760+dexyarya@users.noreply.github.com>
2020-11-28 10:40:47 +0700

    Create 1.cpp

---
+++ 1.cpp
@@ -0,0 +1,34 @@
+
+#include <iostream>
+#include <stdio.h>
+#include <string>
+using namespace std;
+
+
+int main(){
+	int JumlahKalori;
+	double WaktuOlahraga;
+	int n = 20;
+	int a = 2;
+	double x;
+
+	cout<<"Masukan Jumlah Kalori = "<<endl;
+	cin>>JumlahKalori;
+
+    WaktuOlahraga = 2* JumlahKalori / n ;
+	    
+
+	if (JumlahKalori > 750 ){
+		cout<<"Jumlah Kalori = "<<JumlahKalori;
+		cout<<"\n";
+		cout<<"Olahraga = Lari\n";
+	    cout<<"Waktu Olahraga = "<<WaktuOlahraga <<"Menit";
+	}
+	else if(JumlahKalori > 500 ){
+		cout<<"Jumlah Kalori = "<<JumlahKalori;
+		cout<<"\n";
+		cout<<"Olahraga Badminton\n";
+	    cout<<"Waktu Olahraga = "<<WaktuOlahraga <<"Menit";
+	}
+
+}
